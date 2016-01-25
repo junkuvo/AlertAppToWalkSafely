b82ddc7fa4cff8f9df1bbbc49ed6f7f97c1a27a6
junkuvo <0825elle@gmail.com>
2016-01-25 15:41:06 +0900

    位置情報で端末の移動を検知しようとしたバージョン
    結果、STEP_DETECTORのみで歩行を検知できる（手にもって操作していても）ことがわかったのでペンディング

--- AlertAppToWalkSafely/app/src/main/java/apps/junkuvo/alertapptowalksafely/AlertService.java
+++ AlertAppToWalkSafely/app/src/main/java/apps/junkuvo/alertapptowalksafely/AlertService.java
@@ -1,19 +1,28 @@
 package apps.junkuvo.alertapptowalksafely;
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.provider.Settings;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 
 import java.util.List;
 
-public class AlertService extends Service implements SensorEventListener{
+public class AlertService extends Service implements SensorEventListener {
 
     public static final String ACTION = "Alert Service";
     private SensorManager mSensorManager;
@@ -26,6 +35,16 @@ public class AlertService extends Service implements SensorEventListener{
     // 歩数計
     private Sensor mStepDetectorSensor;
     private Sensor mStepCounterSensor;
+
+    // Location
+    private  LocationManager mLocationManager;
+    private List<String> mProviders;
+    private static final float MIN_DISTANCE = 1;// メートル
+    private static final int MIN_INTERVAL = 1;// milisec
+
+    // 歩行中かどうかの判定フラグ
+    boolean isWalking = false;
+    private AlertDialog.Builder mAlertDialog;
 
     @Override
     public void onCreate() {
@@ -53,24 +72,33 @@ public class AlertService extends Service implements SensorEventListener{
         // 歩数計用のセンサー登録
         mStepDetectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
         mStepCounterSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        mSensorManager.registerListener (this, mStepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, mStepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener(this, mStepDetectorSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
 
+        mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        final boolean gpsEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        final boolean netEnabled = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
+        if (!gpsEnabled && !netEnabled) {
+//            // Build an alert dialog here that requests that the user enable
+//            // the location services, then when the user clicks the "OK" button,
+//            // call enableLocationSettings()
+        } else {
+            mProviders =  mLocationManager.getAllProviders();
+            startLocationUpdates();
+        }
+    }
 
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        LocationProvider provider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
-        final boolean gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                10000,          // 10-second interval.
-                10,             // 10 meters.
-                listener);
-
-        if (!gpsEnabled) {
-            // Build an alert dialog here that requests that the user enable
-            // the location services, then when the user clicks the "OK" button,
-            // call enableLocationSettings()
+    private void startLocationUpdates() {
+        if(mLocationManager!=null)
+        {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//                mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, mLocationListener);
+                for(int i = 0 ; i < mProviders.size() ; i++){
+                    mLocationManager.requestLocationUpdates(mProviders.get(i), MIN_INTERVAL, MIN_DISTANCE, mLocationListener);
+                }
+            }
         }
     }
 
@@ -79,18 +107,32 @@ public class AlertService extends Service implements SensorEventListener{
         startActivity(settingsIntent);
     }
 
-    private final LocationListener listener = new LocationListener() {
+    private final LocationListener mLocationListener = new LocationListener() {
 
         @Override
         public void onLocationChanged(Location location) {
             // A new location update is received.  Do something useful with it.  In this case,
             // we're sending the update to a handler which then updates the UI with the new
             // location.
-            Message.obtain(mHandler,
-                    UPDATE_LATLNG,
-                    location.getLatitude() + ", " +
-                            location.getLongitude()).sendToTarget();
+//            Message.obtain(mHandler,
+//                    UPDATE_LATLNG,
+//                    location.getLatitude() + ", " +
+//                            location.getLongitude()).sendToTarget();
+
+            isWalking = true;
+            Log.d("test","test");
         }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+
     };
 
     @Override
@@ -99,28 +141,25 @@ public class AlertService extends Service implements SensorEventListener{
         if (mSensorManager != null) {
             mSensorManager.unregisterListener(this);
         }
-
-        mLocationManager.removeUpdates(listener);
+        if (mLocationManager != null) {
+            if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationManager.removeUpdates(mLocationListener);
+            }
+        }
     }
 
     // BindしたServiceをActivityに返す
     @Override
     public IBinder onBind(Intent intent) {
-//        Toast toast = Toast.makeText(getApplicationContext(), "onBind()", Toast.LENGTH_SHORT);
-//        toast.show();
         return new AlertBinder();
     }
 
     @Override
     public void onRebind(Intent intent) {
-//        Toast toast = Toast.makeText(getApplicationContext(), "onRebind()", Toast.LENGTH_SHORT);
-//        toast.show();
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
-//        Toast toast = Toast.makeText(getApplicationContext(), "onUnbind()", Toast.LENGTH_SHORT);
-//        toast.show();
         return true; // 再度クライアントから接続された際に onRebind を呼び出させる場合は true を返す
     }
 
@@ -145,27 +184,28 @@ public class AlertService extends Service implements SensorEventListener{
             // GPSで移動しているかどうかを判定
             // ある期間移動がなくなったらLocaitonManager remove
             // 再度歩行を検知したらまたLocationManager start
-
-
-            int tendency = mDeviceAttitudeCalculator.calculateDeviceAttitude(event);
-            //　下向きかどうかの判定
-            // 激しく動かすなどするとマイナスの値が出力されることがあるので tendency > 0 とする
-            // さらにテーブルに置いたときなど、水平状態があり得るため tendency > 3(適当) とする
-            if ((tendency > 180 - MainActivity.sAlertStartAngle || tendency < MainActivity.sAlertStartAngle) && tendency > 3) {
-                mTendencyOutCount++;
-                //  下向きと判定されるのが連続5回の場合、Alertを表示させる
-                if(mTendencyOutCount == 5) {
-                    // 歩数計センサの利用：
-                    Intent intent = new Intent(ACTION);
-                    intent.putExtra("isStepCounter",false);
-                    sendBroadcast(intent);
-                    mTendencyOutCount = 0;
+            if(isWalking) {
+                int tendency = mDeviceAttitudeCalculator.calculateDeviceAttitude(event);
+                //　下向きかどうかの判定
+                // 激しく動かすなどするとマイナスの値が出力されることがあるので tendency > 0 とする
+                // さらにテーブルに置いたときなど、水平状態があり得るため tendency > 3(適当) とする
+                if ((tendency > 180 - MainActivity.sAlertStartAngle || tendency < MainActivity.sAlertStartAngle) && tendency > 3) {
+                    mTendencyOutCount++;
+                    //  下向きと判定されるのが連続5回の場合、Alertを表示させる
+                    if (mTendencyOutCount == 5) {
+                        // 歩数計センサの利用：
+                        Intent intent = new Intent(ACTION);
+                        intent.putExtra("isStepCounter", false);
+                        sendBroadcast(intent);
+                        mTendencyOutCount = 0;
+                    }
+                } else {
+                    if (mTendencyOutCount > 0) {
+                        mTendencyOutCount--;
+                        MainActivity.sAlertShowFlag = false;
+                    }
                 }
-            } else {
-                if(mTendencyOutCount > 0) {
-                    mTendencyOutCount--;
-                    MainActivity.sAlertShowFlag = false;
-                }
+                isWalking = false;
             }
             mTendencyCheckCount = 0;
         }
