3c7b10c6f5f46df454f6cf75520a3da9d59833ed
Junkuvo <0825elle@gmail.com>
2016-03-27 23:17:48 +0900

    Set prompt timers

--- AlertAppToWalkSafely/app/src/main/java/apps/junkuvo/alertapptowalksafely/AlertApplication.java
+++ AlertAppToWalkSafely/app/src/main/java/apps/junkuvo/alertapptowalksafely/AlertApplication.java
@@ -3,6 +3,8 @@ package apps.junkuvo.alertapptowalksafely;
 import android.app.Application;
 
 import com.github.stkent.amplify.tracking.Amplify;
+import com.github.stkent.amplify.tracking.PromptViewEvent;
+import com.github.stkent.amplify.tracking.rules.MaximumCountRule;
 
 
 public class AlertApplication extends Application{
@@ -13,13 +15,19 @@ public class AlertApplication extends Application{
     public void onCreate() {
         super.onCreate();
 
-        Amplify.get(this)
-                .setFeedbackEmailAddress("0825elle@gmail.com")
-                .setAlwaysShow(true);
+//        Amplify.get(this)
+//                .setFeedbackEmailAddress("0825elle@gmail.com")
+//                .setAlwaysShow(true);
 //        Amplify.get(this)
 //                .setFeedbackEmailAddress("0825elle@gmail.com")
 //                .applyAllDefaultRules();
-
+        Amplify.get(this)
+                .setFeedbackEmailAddress("0825elle@gmail.com")
+                .setInstallTimeCooldownDays(14) // Prompt not shown within two weeks of initial install.
+                .setLastUpdateTimeCooldownDays(7) // Prompt not shown within one week of most recent update.
+                .setLastCrashTimeCooldownDays(7) // Prompt not shown within one week of most recent crash.
+                .addTotalEventCountRule(PromptViewEvent.USER_GAVE_POSITIVE_FEEDBACK,
+                        new MaximumCountRule(1)); // Never ask the user for feedback again if they already responded positively.
 
     }
 }
