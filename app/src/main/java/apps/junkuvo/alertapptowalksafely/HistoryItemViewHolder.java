0b4efca17fbb6ce35032e2c96bfdcc65460eede5
大久保淳司 <0825elle@gmail.com>
2017-02-04 23:34:27 +0900

    レイアウト調整

---
+++ app/src/main/java/apps/junkuvo/alertapptowalksafely/HistoryItemViewHolder.java
@@ -0,0 +1,11 @@
+package apps.junkuvo.alertapptowalksafely;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class HistoryItemViewHolder extends RecyclerView.ViewHolder {
+
+    public HistoryItemViewHolder(View itemView) {
+        super(itemView);
+    }
+}
